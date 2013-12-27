dad38a457eff20e9816c513899d71be23d2a8543
mikera <mike.r.anderson.13@gmail.com>
2013-12-27 16:08:09 +0000

    basic http-kit server

--- src/main/clojure/mikera/website/core.clj
+++ src/main/clojure/mikera/website/core.clj
@@ -1,3 +1,11 @@
 (ns mikera.website.core
-  )
+  (:require [org.httpkit.server :as hk]))
+
+(defn app 
+  [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "hello HTTP!"})
+
+(hk/run-server #'app {:port 8080})
 
