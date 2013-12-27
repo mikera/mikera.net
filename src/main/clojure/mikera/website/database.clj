db1d1468b3679958cc7bb91dd07efd0595e4933f
mikera <mike.r.anderson.13@gmail.com>
2013-12-27 17:54:43 +0000

    H2 in-memory database code

---
+++ src/main/clojure/mikera/website/database.clj
@@ -0,0 +1,21 @@
+(ns mikera.website.dastabase
+  (:require [clojure.java.jdbc :as j]))
+
+(Class/forName "org.h2.Driver")
+
+(def uri "jdbc:h2:mem:db1;user=;password=;")
+
+(def db {:classname   "org.h2.Driver" ; must be in classpath
+         :subprotocol "h2"
+         :subname "mem:db1"
+         :user     ""
+         :password ""})
+
+
+(defn test []
+  (j/execute! db  ["drop table foo;"])
+  (j/execute! db  ["create table foo (id int, bar char);"])
+  (j/query db ["select * from foo;"])
+  (j/insert! db "foo" {:id 1 :bar "hello"})
+  
+  )
