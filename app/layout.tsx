f314164e58a4d5ce289b2539bcafd17548885186
mikera <mike@mikera.net>
2025-10-27 10:47:41 +0000

    Top level updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,34 @@ import Link from 'next/link'
 export const metadata: Metadata = {
   title: 'mikera.net',
   description: 'Mike Anderson\'s personal homepage',
+  keywords: ['hacker', 'developer', 'open-source', 'economics', 'crypto', 'blockchain', 'data', 'AI', 'freedom'],
+  authors: [{ name: 'mikera', url: 'https://mikera.net' }], 
   icons: {
-    icon: '/mikera.ico',
+    icon: '/mikera-icon.ico',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'mikera',
+    description: 'Economics, crypto, AI and tech insights from mikera.',
+    images: ['/mike-bw.jpg'],
+  },
+  openGraph: {
+    title: 'mikera',
+    description: 'Where code meets creativity. Explore my projects and hacks.',
+    url: 'https://mikera.net', 
+    siteName: 'mikera.net',
+    images: [
+      {
+        url: '/mike-bw.jpg', 
+        alt: 'mikera featured image',
+      },
+    ],
+    locale: 'en_GB',
+    type: 'website',
+  },
+  robots: {
+    index: true, 
+    follow: true
   },
 }
 
@@ -19,7 +45,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/mikera.ico" />
+        <link rel="icon" href="/mikera-icon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Noto+Serif:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
@@ -42,8 +68,8 @@ export default function RootLayout({
           <div>
           </div>
           <div>
-            <p>Follow: <a href="https://x.com/mikera">@mikera</a></p>
-            <p>GitHub: <a href="https://github.com/mikera">mikera</a></p>
+            <p>Follow: <a href="https://x.com/mikera" target="_blank" rel="noopener noreferrer">@mikera</a></p>
+            <p>GitHub: <a href="https://github.com/mikera" target="_blank" rel="noopener noreferrer">mikera</a></p>
           </div>
         </footer>
       </body>
